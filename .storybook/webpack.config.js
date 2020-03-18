3dbd55a8451e2d32df1b577ba18f6337067c21fc
Bilal Haidar <bhaidar@gmail.com>
2020-03-18 22:12:25 +0200

    Refactor code

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -4,17 +4,19 @@ module.exports = {
   module: {
     rules: [
       {
+        // this will apply to both plain `.scss` files
+        // AND `<style lang="scss">` blocks in `.vue` files
         test: /\.scss$/,
         use: [
           {
-            loader: "vue-style-loader",
+            loader: "vue-style-loader"
           },
           {
-            loader: "css-loader",
+            loader: "css-loader"
           },
           {
-            loader: "sass-loader",
-          },
+            loader: "sass-loader"
+          }
         ],
         include: path.resolve(__dirname, "../src")
       }
